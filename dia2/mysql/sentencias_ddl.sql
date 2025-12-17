ebe5d0040e823f2c5b7f36b0fa0e3f6a142cdb40
cmayta <cesarmayta@gmail.com>
2025-12-16 22:01:45 -0500

    sentencias dml

--- dia2/mysql/sentencias_ddl.sql
+++ dia2/mysql/sentencias_ddl.sql
@@ -13,3 +13,10 @@ ADD COLUMN nota INT DEFAULT 0;
 
 -- ELIMINAR UNA TABLA
 DROP TABLE alumno;
+
+CREATE TABLE empresa(  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    ruc VARCHAR(12) NOT NULL UNIQUE,
+    razon_social VARCHAR(255) NOT NULL,
+    direccion TEXT
+);
