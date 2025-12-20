03e31923b73f0a802a4d54d624440b5c2f89446b
cmayta <cesarmayta@gmail.com>
2025-12-20 11:20:46 -0500

    tabulate

--- dia4/api/1-requests.py
+++ dia4/api/1-requests.py
@@ -1,23 +1,23 @@
 import requests
+from tabulate import tabulate
 
-URL = 'https://randomuser.me/api/?results=2'
+URL = 'https://randomuser.me/api/?results=5'
 
 response = requests.get(URL)
 
 if response.status_code == 200:
     print('conexión a api exitosa')
     data = response.json()
+    rows = []
     for dic_user in data['results']:
         nombre = dic_user['name']['first'] + ' ' + dic_user['name']['last']
         pais = dic_user['location']['country']
         email = dic_user['email']
         telefono = dic_user['phone']
         foto = dic_user['picture']['large']
-        print('****************************************')
-        print(f'Nombre  : {nombre}')
-        print(f'Pais    : {pais}')
-        print(f'Email   : {email}')
-        print(f'Telefono: {telefono}')
-        print(f'Foto    : {foto}')
+        rows.append([nombre,pais,email,telefono,foto])
+        
+    headers = ['Nombre','País','Email','Telefóno','Foto']
+    print(tabulate(rows,headers,tablefmt='grid'))
 else:
     print(f'error : {response.status_code}')
