194504dcf3f3b87d781378259f70df515655e1b1
cmayta <cesarmayta@gmail.com>
2025-12-20 11:37:05 -0500

    importar a base de datos

--- dia4/api/1-requests.py
+++ dia4/api/1-requests.py
@@ -1,7 +1,10 @@
 import requests
 from tabulate import tabulate
+import mysql.connector
 
-URL = 'https://randomuser.me/api/?results=5'
+    
+
+URL = 'https://randomuser.me/api/?nat=es&results=100'
 
 response = requests.get(URL)
 
@@ -19,5 +22,41 @@ if response.status_code == 200:
         
     headers = ['Nombre','País','Email','Telefóno','Foto']
     print(tabulate(rows,headers,tablefmt='grid'))
+    
+    # CARGAMOS DATA EN LA BASE DE DATOS
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='root2025',
+        database='db_g6'
+    )
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS usuario(
+            id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            nombre VARCHAR(255) NOT NULL,
+            pais VARCHAR(255) NOT NULL,
+            email VARCHAR(255),
+            telefono VARCHAR(100),
+            foto TEXT
+            );
+            """
+        )
+        #INSERTAMOS LOS USUARIOS A LA BD
+        for usuario in rows:
+            cursor.execute(
+                """
+                insert into usuario(nombre,pais,email,telefono,foto)
+                values(%s,%s,%s,%s,%s)
+                """,
+                usuario
+            )
+        connection.commit()
+        connection.close()
+        print(f' Registros importados a la base de datos')
+    else:
+        print('Error al conectarse a la base de datos')
 else:
     print(f'error : {response.status_code}')
