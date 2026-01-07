f58e9642c059d1ede32ea7eb8be33b183e5d4316
cmayta <cesarmayta@gmail.com>
2026-01-06 21:21:22 -0500

    transform

--- dia5/etl-prefect/main.py
+++ dia5/etl-prefect/main.py
@@ -2,12 +2,15 @@ from tasks.extract import extract
 from tasks.transform import transform
 from tasks.load import load
 from prefect import flow
+from tabulate import tabulate
+
 
 @flow
 def main():
     data = extract()
-    print(data[0])
     data_transform = transform(data)
+    cabeceras = ['nombre','sexo','pais','fecha_nac']
+    print(tabulate(data_transform,headers=cabeceras,tablefmt='grid'))
     load(data_transform)
     
 if __name__ == "__main__":
